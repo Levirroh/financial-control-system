581a486133efed38972bdd31f9162c9b7427f1e9
Levirroh <johann.gr14@gmail.com>
2025-02-09 17:52:48 -0300

    update: reseting stuffs

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -11,31 +11,6 @@ submitButton.addEventListener('click', () => {
         inputs.forEach(input => {
             formData[input.name] = input.value;
 
-            fetch('auth/Register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name_user: formData.name,
-                    email_user: formData.email,
-                    password_user: formData.password,
-                    function: formData.function,
-                    admin: select
-                })
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                if (data.success) {
-                    window.location = data.redirect;
-                } else {
-                    console.log(data.message);
-                }
-            })
-            .catch(error => {
-                console.log('Erro na requisição: ', error);
-            });
         })
     }
 
