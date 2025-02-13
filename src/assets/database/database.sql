2924273be80c48594bdca86f217c3e9898795ac5
Levirroh <johann.gr14@gmail.com>
2025-02-13 11:30:16 -0300

    feat: inserting database in the github repository

---
+++ src/assets/database/database.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE financial_control_system;
+USE financial_control_system;
+
+CREATE TABLE users(
+	id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name_user VARCHAR(45) NOT NULL,
+	password_user TEXT NOT NULL,
+    email_user VARCHAR(90) NOT NULL,
+    isAdmin BOOLEAN,
+    function_user VARCHAR(45)
+);
+
+CREATE TABLE stock(
+	id_item INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name_item VARCHAR(45) NOT NULL,
+    code_item VARCHAR(90) NOT NULL,
+    price_item INT NOT NULL,
+    sale_item BOOLEAN,
+    category_item VARCHAR(45)
+);
+CREATE TABLE requests(
+	id_request INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name_request VARCHAR(45) NOT NULL,
+    code_request VARCHAR(90) NOT NULL,
+    price_request INT NOT NULL,
+    sale_request BOOLEAN,
+    category_request VARCHAR(45)
+);
