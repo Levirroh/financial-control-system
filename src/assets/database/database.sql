a3318b1e7ef4063563f1914df9bc938dc68cb87d
Levirroh <johann.gr14@gmail.com>
2025-02-14 10:01:52 -0300

    minor changes on database

--- src/assets/database/database.sql
+++ src/assets/database/database.sql
@@ -6,8 +6,8 @@ CREATE TABLE users(
     name_user VARCHAR(45) NOT NULL,
 	password_user TEXT NOT NULL,
     email_user VARCHAR(90) NOT NULL,
-    isAdmin BOOLEAN,
-    function_user VARCHAR(45)
+    isAdmin BOOLEAN NOT NULL,
+    function_user VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE stock(
@@ -16,13 +16,15 @@ CREATE TABLE stock(
     code_item VARCHAR(90) NOT NULL,
     price_item INT NOT NULL,
     sale_item BOOLEAN,
-    category_item VARCHAR(45)
+    category_item VARCHAR(45) NOT NULL
 );
 CREATE TABLE requests(
 	id_request INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name_request VARCHAR(45) NOT NULL,
     code_request VARCHAR(90) NOT NULL,
-    price_request INT NOT NULL,
-    sale_request BOOLEAN,
-    category_request VARCHAR(45)
+    category_request VARCHAR(45) NOT NULL,
+    approved_request ENUM ("Aprovado", "Recusado", "Não visto")
 );
+
+
+SELECT * FROM requests;
