46707dfdf14ef4d37ac2ef18974bcc64c1fd0e09
Levirroh <johann.gr14@gmail.com>
2025-02-21 09:52:22 -0300

    feat: first graphic

--- src/App/View/admin/graphics.php
+++ src/App/View/admin/graphics.php
@@ -16,18 +16,19 @@
       function drawChart() {
 
         // Create the data table.
+        var rawData = {{ data | raw }}; // data do PHP
+
+
+
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
+        data.addColumn('string', 'Item');
+        data.addColumn('number', 'Qtd vendida');
+        rawData.forEach(function(row) {
+          data.addRow([row.name_item, parseInt(row['COUNT(fk_item)'])]);
+        });
+
         // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
+        var options = {'title':'Transações de itens',
                        'width':400,
                        'height':300};
 
