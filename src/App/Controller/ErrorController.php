875e4126d6b2be06168cc0a4e6318d28b56c5eac
Levirroh <johann.gr14@gmail.com>
2025-02-24 11:33:32 -0300

    feat: error page is now a page and changes if you are an admin or not

--- src/App/Controller/ErrorController.php
+++ src/App/Controller/ErrorController.php
@@ -2,7 +2,25 @@
 
 Class ErrorController{
     public function index(){
-        echo 'Página de Erro';
+        try {
+            $loader = new \Twig\Loader\FilesystemLoader('../src/App/View/user');
+            $twig = new \Twig\Environment($loader); 
+
+            $admin = $_SESSION['isAdmin'];
+
+            if ($admin){
+                $template = $twig->load('errorAdmin.html'); 
+            } else{
+                $template = $twig->load('error.html'); 
+
+            }
+
+
+            $conteudo = $template->render();
+            echo $conteudo;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
 
